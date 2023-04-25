7355a133196a288b502db365d796dc2a3b49aab0
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-04-25 22:10:50 +0530

    Sign in edit

--- src/components/SignIn/SignInPage.jsx
+++ src/components/SignIn/SignInPage.jsx
@@ -3,7 +3,28 @@ function SignInPage() {
       <div className="container">
          <h1>Sign in here</h1>
 
-         <div>SCA</div>
+         <form>
+            <div className="form-group">
+               <label htmlFor="email">Email Address</label>
+               <input
+                  type="email"
+                  id="email"
+                  placeholder="Email Address"
+                  className="form-control"
+               />
+            </div>
+            <div className="form-group">
+               <label htmlFor="pw">Password</label>
+               <input
+                  type="password"
+                  id="pw"
+                  placeholder="Password"
+                  className="form-control"
+               />
+            </div>
+
+            <button className="btn btn-primary">Sign In</button>
+         </form>
       </div>
    );
 }
