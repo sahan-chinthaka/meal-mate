0393c29d2dc86372742bb22b2aefcfbac8c384fa
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-04-25 23:36:48 +0530

    Sign in changes

--- src/components/SignIn/SignInPage.jsx
+++ src/components/SignIn/SignInPage.jsx
@@ -1,15 +1,29 @@
 import { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase_init";
 
 function SignInPage() {
    const [email, setEmail] = useState("");
    const [pw, setPw] = useState("");
+   const [error, setError] = useState(null);
+   const navigate = useNavigate();
+
+   function signIn(e) {
+      e.preventDefault();
+      setError(null);
+      signInWithEmailAndPassword(auth, email, pw)
+         .then(() => navigate("/"))
+         .catch((error) => {
+            setError(error.message);
+         });
+   }
 
    return (
       <div className="container">
          <h1>Sign in here</h1>
 
-         <form>
+         <form onSubmit={signIn}>
             <div className="form-group">
                <label htmlFor="email">Email Address</label>
                <input
@@ -32,6 +46,8 @@ function SignInPage() {
                   value={pw}
                />
             </div>
+
+            {error && <div className="alert alert-danger">{error}</div>}
 
             <button type="submit" className="btn btn-primary">
                Sign In
