76ee064fed09a1d104d0fae0a898a6a6bb9bd21a
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-05-01 13:59:23 +0530

    Improve sign up function

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,9 +1,32 @@
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import OwnerView from "./OwnerView";
+import UserView from "./UserView";
+import { doc, getDoc } from "firebase/firestore";
+import { fs } from "../../firebase_init";
+import { useEffect, useState } from "react";
 
 function HomePage() {
    const auth = useAuth();
+   const [user, setUser] = useState();
 
-   return <div>{JSON.stringify(auth)}</div>;
+   useEffect(() => {
+      if (auth === undefined || auth === null) return;
+      var d = doc(fs, "users", auth.uid);
+      getDoc(d).then((dc) => {
+         setUser(dc.data());
+      });
+   }, [auth]);
+
+   if (auth === null) {
+      return <Navigate to="/sign_in" />;
+   }
+   if (user == undefined) return "Loading...";
+
+   if (user.type == "user") {
+      return <UserView />;
+   }
+   return <OwnerView />;
 }
 
 export default HomePage;
---
+++ src/components/Home/OwnerView.jsx
@@ -0,0 +1,10 @@
+function OwnerView() {
+   return (
+      <div className="container">
+         <h2>Your shops</h2>
+         <button>Add shop</button>
+      </div>
+   );
+}
+
+export default OwnerView;
