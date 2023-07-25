6a0bb2bc9adbdd08e8e2de85ac0e2a299f62b184
Sahan Chinthaka <sahanchinthaka04@gmail.com>
2023-07-25 22:57:24 +0530

    Created owner sign up

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -7,9 +7,10 @@ import Cities from "./cities.json";
 import { auth, fs } from "../../firebase";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-	const [view, setView] = useState(1);
+	const [view, setView] = useState(2);
 
 	return (
 		<div className="container">
@@ -37,11 +38,11 @@ function UserSignUp() {
 	const [err, setErr] = useState(null);
 	const [dis, setDis] = useState(false);
 	const form = useRef(null);
+	const navigate = useNavigate();
 
 	function submit(e) {
 		e.preventDefault();
 		setErr(null);
-		setDis(true);
 		const email = form.current[0].value;
 		const name = form.current[1].value;
 		const dis = form.current[2].value;
@@ -52,6 +53,7 @@ function UserSignUp() {
 		if (name.length <= 3) return setErr("Enter a valid name");
 		if (pw.length < 6) return setErr("Password minimum length should be 6");
 		if (pw !== pwc) return setErr("Passwords are not same");
+		setDis(true);
 
 		createUserWithEmailAndPassword(auth, email, pw)
 			.then((cred) => {
@@ -72,7 +74,9 @@ function UserSignUp() {
 					city: cityN,
 					lati: c["d"],
 					long: c["e"],
-				});
+				})
+					.then(() => navigate("/"))
+					.catch((err) => setErr(err.code));
 			})
 			.catch((err) => {
 				if (err.code == "auth/email-already-in-use") return setErr("Email already in use");
@@ -142,7 +146,125 @@ function UserSignUp() {
 }
 
 function OwnerSignUp() {
-	return "Owner";
+	const [dId, setDID] = useState(1);
+	const [err, setErr] = useState(null);
+	const [dis, setDis] = useState(false);
+	const form = useRef(null);
+	const navigate = useNavigate();
+
+	function submit(e) {
+		e.preventDefault();
+		setErr(null);
+
+		const email = form.current[0].value;
+		const shopName = form.current[1].value;
+		const shopContact = form.current[2].value;
+		const shopAddress = form.current[3].value;
+		const dis = form.current[4].value;
+		const city = form.current[5].value;
+		const pw = form.current[6].value;
+		const pwc = form.current[7].value;
+
+		if (shopName.length < 2) return setErr("Enter a valid name");
+		if (pw.length < 6) return setErr("Password minimum length should be 6");
+		if (pw !== pwc) return setErr("Passwords are not same");
+
+		const d = Districts[dis - 1];
+		const c = Cities.filter((i) => i.a == dis)[city];
+		const disN = d["name_en"] + " " + d["name_si"];
+		const cityN = c["b"] + " " + c["c"];
+		setDis(true);
+
+		createUserWithEmailAndPassword(auth, email, pw)
+			.then((cred) => {
+				const user = cred.user;
+
+				setDoc(doc(fs, "shops", user.uid), {
+					email,
+					shopName,
+					district: disN,
+					city: cityN,
+					shopContact,
+					shopAddress,
+					lati: c["d"],
+					long: c["e"],
+				})
+					.then(() => navigate("/"))
+					.catch((err) => setErr(err.code));
+			})
+			.catch((err) => {
+				if (err.code == "auth/email-already-in-use") return setErr("Email already in use");
+				if (err.code == "auth/invalid-email") return setErr("Invalid email address");
+				return setErr(err.code);
+			})
+			.finally(() => {
+				setDis(false);
+			});
+	}
+
+	return (
+		<div>
+			<form onSubmit={submit} ref={form}>
+				<div className="form-group">
+					<label htmlFor="email">Email Address</label>
+					<input required type="email" name="email" id="email" placeholder="Email" className="form-control" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="shop-name">Shop Name</label>
+					<input required type="text" name="shop-name" id="shop-name" placeholder="Name" className="form-control" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="shop-contact">Shop Contact Number</label>
+					<input type="text" name="shop-contact" id="shop-contact" placeholder="Contact" className="form-control" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="shop-address">Shop Address</label>
+					<textarea
+						required
+						name="shop-address"
+						id="shop-address"
+						rows="5"
+						className="form-control"
+						placeholder="Address"
+					></textarea>
+				</div>
+				<div className="form-group">
+					<label htmlFor="distric">District</label>
+					<select name="distric" id="distric" className="form-control" onChange={(e) => setDID(e.target.value)}>
+						{Districts.map((i) => (
+							<option key={i.id} value={i.id}>
+								{i.name_en} - {i.name_si}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="form-group">
+					<label htmlFor="city">City</label>
+					<select name="city" id="city" className="form-control">
+						{Cities.filter((i) => i.a == dId).map((i, k) => (
+							<option key={k} value={k}>
+								{i.b} - {i.c}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="row">
+					<div className="form-group col">
+						<label htmlFor="pw">Password</label>
+						<input required name="pw" type="password" className="form-control" id="pw" placeholder="Password" />
+					</div>
+					<div className="form-group col">
+						<label htmlFor="pwc">Password Confirm</label>
+						<input required name="pwc" type="password" className="form-control" id="pwc" placeholder="Password" />
+					</div>
+				</div>
+				{err !== null && <div className="alert alert-danger">{err}</div>}
+				<button disabled={dis} type="submit" className="btn btn-primary">
+					Sign Up
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default SignUp;
