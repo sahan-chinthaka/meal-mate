945c58804655637a4305fcfda69e19b2b29c9f3d
sahan-chinthaka <sahanchinthaka04@gmail.com>
2023-07-30 20:58:51 +0530

    Created shop view for users

--- src/components/User/Shop/Shop.jsx
+++ src/components/User/Shop/Shop.jsx
@@ -1,27 +1,85 @@
-import { collection, doc, getDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { getDownloadURL, ref } from "firebase/storage";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FS, Storage } from "../../../firebase";
-import { getDownloadURL, ref } from "firebase/storage";
+import "./shop.scss";
 
 function Shop() {
 	const { shopID } = useParams();
 	const [data, setData] = useState({});
+	const [selected, setSelected] = useState(0);
+	const [foodList, setFoodList] = useState([]);
 
 	useEffect(() => {
 		const d = doc(FS, "shops", shopID);
-		getDoc(d).then((snap) => {
-			setData(snap.data());
+		getDoc(d)
+			.then((snap) => {
+				setData(snap.data());
 
-			const r = ref(Storage, "shop/" + shopID);
-			getDownloadURL(r).then((url) => setData((d) => ({ ...d, url })));
+				const r = ref(Storage, "shop/" + shopID);
+				getDownloadURL(r).then((url) => setData((d) => ({ ...d, url })));
+			})
+			.catch(() => setData(null));
+
+		const c = collection(FS, "shops", shopID, "foods");
+		getDocs(c).then((snap) => {
+			setFoodList(
+				snap.docs.map((i) => ({
+					...i.data(),
+					id: i.id,
+				}))
+			);
 		});
 	}, []);
 
+	if (data == null) return "Not found";
+
 	return (
-		<div className="container">
-			<h1>{data.shopName}</h1>
-			<img src={data.url} width="100" />
+		<div className="container shop-container">
+			<div className="shop-header" style={{ backgroundImage: `url("${data.url}")` }}>
+				<div className="grad">
+					<h1 align="center">{data.shopName}</h1>
+				</div>
+			</div>
+			<div className="tab-holder">
+				<div onClick={() => setSelected(0)} className={selected == 0 ? "tab-selected" : ""}>
+					Foods
+				</div>
+				<div onClick={() => setSelected(1)} className={selected == 1 ? "tab-selected" : ""}>
+					Reviews
+				</div>
+			</div>
+			{selected == 0 && <FoodView shopID={shopID} foodList={foodList} />}
+		</div>
+	);
+}
+
+function FoodView({ foodList, shopID }) {
+	return (
+		<div className="food-items">
+			{foodList.map((i) => (
+				<FoodItemView key={i.id} data={i} shopID={shopID} />
+			))}
+		</div>
+	);
+}
+
+function FoodItemView({ data, shopID }) {
+	const [url, setURL] = useState(null);
+	useEffect(() => {
+		const r = ref(Storage, "foods/" + shopID + "/" + data.id);
+		getDownloadURL(r).then(setURL);
+	}, []);
+	return (
+		<div className="food-item-view">
+			<div className="food-image">
+				<img src={url} alt="" />
+			</div>
+			<div className="food-details">
+				<h3>{data.name}</h3>
+				<p>{data.description}</p>
+			</div>
 		</div>
 	);
 }
